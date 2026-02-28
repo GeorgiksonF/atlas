de21e7e43d06167adc34049777b3f93bf0206420
DESKTOP-7LJI4GG\georg <Falyan.G@digdes.com>
2026-02-28 16:17:43 +0400

    feat(types): в типы portfolios добавлено поле broker

--- libs/types/src/portfolios.ts
+++ libs/types/src/portfolios.ts
@@ -4,18 +4,21 @@ export type CreatePortfolio = {
 	name: string;
 	description?: string;
 	category?: PortfolioCategory;
+	broker?: string;
 };
 
 export type UpdatePortfolio = {
 	name?: string;
 	description?: string;
 	category?: PortfolioCategory;
+	broker?: string;
 };
 
 export type PortfolioResponse = {
 	id: string;
 	name: string;
 	category: PortfolioCategory;
+	broker: string | null;
 	createdAt: Date;
 	updatedAt: Date;
 	description: string | null;
